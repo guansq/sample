d8ed192428d591904274f20ffc8fd0e7cd3ab9d4
vagrant <vagrant@homestead>
2017-12-06 06:04:22 +0000

    fix bug

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,12 +2,6 @@
 @section('content')
     <div class="jumbotron">
         <h1>hello damei</h1>
-        <p class="lead">
-            三婶摸男人三婶摸男人 <a href="">我的占位符</a>
-        </p>
-        <p>
-            从头开始
-        </p>
         <p>
             <a class="btn btn-lg btn-success" role="button" href="{{ route('signup') }}">现在注册</a>
         </p>
